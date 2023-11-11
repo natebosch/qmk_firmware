b99d55adc51a0abdde35fa27d338d428b8acff33
Nate Bosch <nbosch1@gmail.com>
2023-11-11 12:11:27 -0800

    Extra mouse buttons on left hand
    Allow clicking with left hand while right hand is on a trackpad. This is
    currently a workaround for broken right click on the laptop, but it
    also feels pretty comfortable, especially with the layer locked.

--- keyboards/controllerworks/mini42/keymaps/natebosch/keymap.c
+++ keyboards/controllerworks/mini42/keymaps/natebosch/keymap.c
@@ -192,7 +192,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Arrows and mouse keys
   [l_arw] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------------.                      ,-----------------------------------------------------------.
-       XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  SFT_ALT,  XXXXXXX,                         KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  XXXXXXX,  XXXXXXX,
+       XXXXXXX,  XXXXXXX,  KC_BTN1,  KC_BTN2,  SFT_ALT,  XXXXXXX,                         KC_LEFT,  KC_DOWN,    KC_UP,  KC_RGHT,  XXXXXXX,  XXXXXXX,
   //|---------+---------+---------+---------+---------+---------|                      |---------+---------+---------+---------+---------+---------|
        XXXXXXX,TT(l_arw),  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_LSFT,                         KC_MS_L,  KC_MS_D,  KC_MS_U,  KC_MS_R,   KC_TAB,  XXXXXXX,
   //|---------+---------+---------+---------+---------+---------|                      |---------+---------+---------+---------+---------+---------|
