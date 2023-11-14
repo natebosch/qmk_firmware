8c49c0ad047205862be31d7d7198a5473af62481
Nate Bosch <nbosch1@gmail.com>
2023-11-13 20:30:37 -0800

    Increase default tapping term

--- keyboards/controllerworks/mini42/keymaps/natebosch/config.h
+++ keyboards/controllerworks/mini42/keymaps/natebosch/config.h
@@ -20,7 +20,7 @@
 #define MASTER_RIGHT
 #define TAPPING_TOGGLE 1
 #define TAP_CODE_DELAY 3
-#define TAPPING_TERM 150
+#define TAPPING_TERM 160
 
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
