03e3e87add2585655723e74d5d708ef65dbe1d6c
Nate Bosch <nbosch1@gmail.com>
2024-09-15 01:39:00 -0700

    Add RGB layer key hints
    Add a map of colors for each key with a function on each layer above the
    base layer.
    Add a RGB matrix indicator callback which reads the array and assigns
    colors for each key in range. Scale the colors by the brightness. Divide
    in 2 again when using the heatmap RGB effect since those colors are more
    dim. On other effects the brightness appears higher still for the hint
    keys, I am not sure why. Using the halved approach makes a close match,
    but leaving the hints brighter is more legible when the RGB effect is
    noisy.
    
    Increase the ramp rate for the heatmap effect.
    React to key releases instead of presses.
    Lower RGB effect maximum brightness (even though I'm not sure I noticed
    the difference).
    
    Swap the volume up/down so that down is next to mute.
    
    Swap RGB saturation up/down since there effect is more like the opposite
    of the value (brightness) changing.

--- keyboards/controllerworks/mini42/keymaps/natebosch/config.h
+++ keyboards/controllerworks/mini42/keymaps/natebosch/config.h
@@ -33,12 +33,12 @@
 #define EE_HANDS
 
 #ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#    define RGB_MATRIX_KEYRELEASES // reacts to keypresses
 #    define RGB_MATRIX_TIMEOUT 120000 // number of ticks to wait until disabling effects
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 #    define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100 // out of 255. Higher may cause the controller to crash.
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
 #    define RGB_MATRIX_VAL_STEP 8
@@ -47,6 +47,7 @@
 #    define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
 #    define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
 #    define ENABLE_RGB_MATRIX_TYPING_HEATMAP
+#    define RGB_MATRIX_TYPING_HEATMAP_INCREASE_STEP 128
 
 #    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_TYPING_HEATMAP
 #endif
